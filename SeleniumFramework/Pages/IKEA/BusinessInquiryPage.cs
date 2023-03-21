11bf005881fbcd7f98154e281eeefdd211e0efc9
MartynasVCS <martynasvcs@gmail.com>
2023-03-22 00:10:30 +0200

    Refactor method names in business client request form scenario

--- SeleniumFramework/Pages/IKEA/BusinessInquiryPage.cs
+++ SeleniumFramework/Pages/IKEA/BusinessInquiryPage.cs
@@ -12,37 +12,37 @@ namespace SeleniumFramework.Pages.IKEA
             Driver.OpenUrl("https://www.ikea.lt/lt/business/inquiry");
         }
 
-        public static void InputWrongRepresentativeName(string name)
+        public static void EnterWrongRepresentativeName(string name)
         {
             string locator = "//*[@name='representative_name']";
             Common.SendKeys(locator, name);
         }
 
-        public static void InputRepresentativeSurname(string surname)
+        public static void EnterRepresentativeSurname(string surname)
         {
             string locator = "//*[@name='representative_surname']";
             Common.SendKeys(locator, surname);
         }
 
-        public static void InputCompanyName(string companyName)
+        public static void EnterCompanyName(string companyName)
         {
             string locator = "//*[@name='company_name']";
             Common.SendKeys(locator, companyName);
         }
 
-        public static void InputEmail(string email)
+        public static void EnterEmail(string email)
         {
             string locator = "//*[@name='email']";
             Common.SendKeys(locator, email);
         }
 
-        public static void InputPhone(string phone)
+        public static void EnterPhone(string phone)
         {
             string locator = "//*[@name='phone']";
             Common.SendKeys(locator, phone);
         }
 
-        public static void InputRequest(string request)
+        public static void EnterRequest(string request)
         {
             string locator = "//*[@id='root']/div[4]/div/textarea";
             Common.SendKeys(locator, request);
